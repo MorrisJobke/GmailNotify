5f4562bfff1b6b4841dba808979bca1579bf8412
kabum <uu.kabum@gmail.com>
2010-05-14 13:09:24 +0200

    Adding menu entry

--- build-deb.sh
+++ build-deb.sh
@@ -2,16 +2,21 @@
 
 mkdir -p build-deb/usr/lib/gmailnotify
 cp gmailnotify.py build-deb/usr/lib/gmailnotify
-chmod +x build-deb/usr/lib/gmailnotify/gmailnotify.py
 cp gmailnotifykeyring.py build-deb/usr/lib/gmailnotify
 cp gmailnotify-settings.py build-deb/usr/lib/gmailnotify
-cp gmailnotify.desktop build-deb/usr/lib/gmailnotify
+
 cp gmailnotify-settings.glade build-deb/usr/lib/gmailnotify		
+
+chmod +x build-deb/usr/lib/gmailnotify/gmailnotify.py
 chmod +x build-deb/usr/lib/gmailnotify/gmailnotify-settings.py
+
 mkdir -p build-deb/usr/share/applications
+cp gmailnotify.desktop build-deb/usr/share/applications
 cp gmailnotify-settings.desktop build-deb/usr/share/applications
+
 mkdir -p build-deb/usr/share/pixmaps
 cp gmailnotify.svg build-deb/usr/share/pixmaps
+
 mkdir -p build-deb/usr/bin
 ln -s ../lib/gmailnotify/gmailnotify.py build-deb/usr/bin/gmailnotify
 ln -s ../lib/gmailnotify/gmailnotify-settings.py build-deb/usr/bin/gmailnotify-settings
